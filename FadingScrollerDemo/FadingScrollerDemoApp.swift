4bddfe06ac443eeacf8acfa43874fad6a3aa4a11
Scott Andrew <scott@nwdm.tech>
2022-10-05 19:18:02 -0700

    Initial Commit

---
+++ FadingScrollerDemo/FadingScrollerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FadingScrollerDemoApp.swift
+//  FadingScrollerDemo
+//
+//  Created by Scott Andrew on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FadingScrollerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
